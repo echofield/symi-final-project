252506ba74bb4d56143a59d674b39eb437640ee4
echofield <echofield@outlook.fr>
2025-08-05 10:55:26 +0300

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import dynamic from 'next/dynamic';
 import { Check, ChevronDown } from 'lucide-react';
 
